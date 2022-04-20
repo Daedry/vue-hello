8ddcd3f0fdfeb40205745deb56d0f52b0f85be55
MyUsername <romamargine@gmail.com>
2022-04-20 14:40:57 +0200

    Add finish es

---
+++ app.js
@@ -0,0 +1,13 @@
+/* 
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const app = new Vue({
+    el:'#app',
+    data:{
+        text:'Sto imparando Vuejs',
+    }
+})
